100eb685b08ef28faa5712ccbe00fce0ff5c07f4
igarashi-naoki <naoki17igarashi@gmail.com>
2023-09-20 08:37:24 +0900

    デフォルトロケールやパラメータからのロケール取得に関する設定を変更

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
+
   end
 end
